2505f8ee773e8867edf5ed4ddf7023166ba6896d
mak199 <mak.khan199@googlemail.com>
2021-03-05 14:49:49 +0100

    Handling Allocation Requests. closes #8

--- src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Component;
 public class JmsConfig {
 
     public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+    public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
+    public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order-response";
 
     // Serialize message content to json using TextMessage
     @Bean
