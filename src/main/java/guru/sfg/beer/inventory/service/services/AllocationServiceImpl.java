9c1974680571545732fd263e8a6fa5ccad271c0a
mak199 <mak.khan199@googlemail.com>
2021-03-10 15:58:48 +0100

    Removing zero on hand inventory records. closes #11

--- src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
@@ -72,6 +72,9 @@ public class AllocationServiceImpl implements AllocationService {
                 beerOrderLine.setQuantityAllocated(allocatedQty + inventory);
                 beerInventory.setQuantityOnHand(0);
 
+                //beerInventoryRepository.delete(beerInventory);
+            }
+            if(beerInventory.getQuantityOnHand()==0){
                 beerInventoryRepository.delete(beerInventory);
             }
         });
